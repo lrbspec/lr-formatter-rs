611e04557afb4deada0927e60bd336e12bd6965d
Malizma333 <malizma333@gmail.com>
2025-06-18 10:24:50 -0500

    changed track count function to return u32

--- lib/src/formats/sol.rs
+++ lib/src/formats/sol.rs
@@ -5,16 +5,15 @@ mod writer;
 pub use reader::read;
 pub use writer::write;
 
-use anyhow::Result;
 use byteorder::{BigEndian, ReadBytesExt};
 use std::io::{Cursor, Seek};
 
-pub fn get_track_count(data: &[u8]) -> Result<u32> {
+pub fn get_track_count(data: &[u8]) -> u32 {
     let mut cursor = Cursor::new(data);
 
     // HACK: We assume header size is constant, and track list length will always be at 0x2C - 0x2F
-    cursor.seek(std::io::SeekFrom::Start(0x2C))?;
-    let num_tracks = cursor.read_u32::<BigEndian>()?;
+    let _ = cursor.seek(std::io::SeekFrom::Start(0x2C));
+    let num_tracks = cursor.read_u32::<BigEndian>().unwrap_or(0);
 
-    Ok(num_tracks)
+    num_tracks
 }
