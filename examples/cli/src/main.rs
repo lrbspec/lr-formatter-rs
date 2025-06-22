1c5e3a58763fd8df6fbf8aae017d4af918d6b3fa
Malizma333 <malizma333@gmail.com>
2025-06-22 09:02:13 -0500

    localized conversion function

--- examples/cli/src/main.rs
+++ examples/cli/src/main.rs
@@ -1,8 +1,7 @@
 use anyhow::{Context, Result};
 use clap::Parser;
 use dialoguer::Input;
-use lr_formatter_rs::sol;
-use lr_formatter_rs::{Format, convert};
+use lr_formatter_rs::{lrb, sol, trackjson, trk};
 use std::fs::File;
 use std::io::{Read, Write};
 use std::path::Path;
@@ -20,6 +19,39 @@ struct Cli {
     output_format: String,
     /// Optional output file path
     output_file: Option<String>,
+}
+
+enum Format {
+    TrackJson,
+    LRB,
+    TRK,
+    SOL(Option<u32>),
+}
+
+fn convert(input: &[u8], from: Format, to: Format) -> Result<Vec<u8>> {
+    let internal_format = match from {
+        Format::TrackJson => {
+            let input_str = String::from_utf8(input.to_vec())?;
+            trackjson::read(&input_str)?
+        }
+        Format::LRB => lrb::read(input)?,
+        Format::TRK => trk::read(input)?,
+        Format::SOL(track_index) => sol::read(input, track_index)?,
+    };
+
+    let output_bytes = match to {
+        Format::TrackJson => {
+            let json_str = trackjson::write(&internal_format)?;
+            Ok(json_str.into_bytes())
+        }
+        Format::LRB => lrb::write(&internal_format),
+        Format::SOL(_) => sol::write(&internal_format),
+        _ => Err(anyhow::anyhow!(
+            "Unsupported to format. Must be one of: trackjson, lrb, sol",
+        )),
+    };
+
+    output_bytes
 }
 
 fn parse_format(format: &str, sol_index: Option<u32>) -> Result<Format> {
