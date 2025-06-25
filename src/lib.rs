8cbe8d824984962c509cb9e88bdb3e73cc64ce20
Malizma333 <malizma333@gmail.com>
2025-06-24 20:52:55 -0500

    updated library root doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
-//! This crate allows reading and writing Line Rider track file formats\
+//! This crate provides safe reading and writing of Line Rider track file formats\
 //! Supported formats: LRB (read/write), SOL (read/write), JSON (read/write), TRK (read)
 //!
 //! # Usage
 //! ```rust
-//! let track_bytes = fs::read_to_string("test.track.json").unwrap();
-//! let track = lr_formatter_rs::trackjson::read(&track_bytes).unwrap();
+//! // Note: `.unwrap()` is used here for example purposes only
+//! let track_json_bytes = fs::read_to_string("test.track.json").unwrap();
+//! let track = lr_formatter_rs::trackjson::read(&track_json_bytes).unwrap();
 //! println!("Title: {}", track.title);
+//! let track_lrb_bytes = lr_formatter_rs::lrb::write(&track).unwrap();
+//! fs::File::create("test_track.lrb").unwrap().write_all(track_lrb_bytes).unwrap()
 //! ```
 
 mod errors;
